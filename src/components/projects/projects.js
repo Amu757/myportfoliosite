07a7fc05fb40a0511cf8524752d0d3f9f9dbe5f4
Aman Waghmare <amanwaghmare311@gmail.com>
2024-08-11 02:13:23 +0530

    added new project blognest

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -2,22 +2,24 @@ import React from "react";
 import "../homepage.css";
 import Projectbox from "./Projectbox";
 const ecomvid = require("../../projectimages/ecommerce/ecomm_video.mp4");
+const blognestvid = require("../../projectimages/blognest/blognest_vid.mp4");
 const ecomlink = "https://ecommerce-site-ten-theta.vercel.app/";
-
-const BlogNestlink = "https://blog-nest-blue.vercel.app/"
+const BlogNestlink = "https://blog-nest-blue.vercel.app/";
 
 export default function projects() {
   return (
     <div className="" id="projectcontainer">
       <h1 id="title" className="projectstop">
-        My <span className="spanword" id="projectssec">Projects</span>
+        My{" "}
+        <span className="spanword" id="projectssec">
+          Projects
+        </span>
       </h1>
       <div className="" id="projectbox-container">
-        <Projectbox vid={ecomvid} link={ecomlink}/>
-        <Projectbox vid={ecomvid} link={BlogNestlink}/>
-        <Projectbox vid={ecomvid} link={ecomlink}/>
-        <Projectbox vid={ecomvid} link={ecomlink}/>
-
+        <Projectbox vid={ecomvid} link={ecomlink} />
+        <Projectbox vid={blognestvid} link={BlogNestlink} />
+        <Projectbox vid={ecomvid} link={ecomlink} />
+        <Projectbox vid={ecomvid} link={ecomlink} />
       </div>
     </div>
   );
