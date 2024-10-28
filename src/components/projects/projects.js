a2e82d1f5a88643edb95a03c4ea5be56b6f14dce
Aman Waghmare <amanwaghmare311@gmail.com>
2024-10-28 08:55:03 +0530

    removed curruncy project

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -3,13 +3,11 @@ import "../homepage.css";
 import Projectbox from "./Projectbox";
 const ecomvid = require("../../projectimages/ecommerce/ecomm_video.mp4");
 const blognestvid = require("../../projectimages/blognest/blognest_vid.mp4");
-const currencyvid = require("../../projectimages/currency/currencyconvertor vid.mp4");
 const movieratingvid = require("../../projectimages/movierating/movieratingvid.mp4");
 const studytubevid = require("../../projectimages/studytube/studytube_vid.mp4")
 
 const ecomlink = "https://ecommerce-site-ten-theta.vercel.app/";
 const BlogNestlink = "https://blog-nest-blue.vercel.app/";
-const currencylink = "https://currency-converter-seven-woad.vercel.app/";
 const movieratinglink = "https://movie-ratings-lake.vercel.app/";
 const studytubelink = "https://studytube-frontend.vercel.app/"
 
@@ -27,7 +25,6 @@ export default function projects() {
         <Projectbox vid={ecomvid} link={ecomlink} />
         <Projectbox vid={blognestvid} link={BlogNestlink} />
         <Projectbox vid={movieratingvid} link={movieratinglink} />
-        <Projectbox vid={currencyvid} link={currencylink} />
       </div>
     </div>
   );
